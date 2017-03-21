206d3d2ec40a7b9efea894e7cdae9ed94107fc21
Jay Cribas <feverjay@gmail.com>
2017-03-21 13:43:01 -0700

    Khan Academy day 1 exercises completed

---
+++ khan/sql-basics-1.sql
@@ -0,0 +1,14 @@
+/* Challenge: Book list database
+
+STEP 1: What are your favorite books? You can make a database table to store them in! In this first step, create a table to store your list of books. It should have columns for id, name, and rating.
+
+STEP 2: Now, add three of your favorite books into the table.
+*/
+
+/* STEP 1 */
+CREATE TABLE books (id INTEGER PRIMARY KEY, name TEXT, rating INTEGER);
+
+/* STEP 2 */
+INSERT INTO books VALUES (1, "Harry Potter", 8);
+INSERT INTO books VALUES (2, "Game of Thrones", 9);
+INSERT INTO books VALUES (3, "Jurrasic Park", 7);
---
+++ khan/sql-basics-3.sql
@@ -0,0 +1,16 @@
+/* Challenge: TODO list database stats
+
+STEP 1: Here's a table containing a TODO list with the number of minutes it will take to complete each item. Insert another item to your todo list with the estimated minutes it will take.
+
+STEP 2: Select the SUM of minutes it will take to do all of the items on your TODO list. You should only have one SELECT statement.
+*/
+
+CREATE TABLE todo_list (id INTEGER PRIMARY KEY, item TEXT, minutes INTEGER);
+INSERT INTO todo_list VALUES (1, "Wash the dishes", 15);
+INSERT INTO todo_list VALUES (2, "vacuuming", 20);
+INSERT INTO todo_list VALUES (3, "Learn some stuff on KA", 30);
+/* STEP 1 */
+INSERT INTO todo_list VALUES (4, "Laundry", 60);
+
+/* STEP 2 */
+SELECT SUM(minutes) FROM todo_list;
